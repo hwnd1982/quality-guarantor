5a15e2cb12fdf9987e31a8ce5f8871c9bbc1f304
Kirill Lavrov <lkv@myttk.ru>
2023-12-03 02:09:51 +0700

    fixed placeholder control

--- js/formInputHandler.js
+++ js/formInputHandler.js
@@ -2,10 +2,10 @@ export const formInputHandler = () => {
   const forms = document.querySelectorAll('.form');
 
   forms.forEach(form => {
-    form.addEventListener('blur', ({target}) => {
-      const input = target.closest('.input-item');
-
-      if (input) return;
+    form.addEventListener('change', ({target}) => {
+      const input = target.closest('.form__input-item');
+      
+      if (!input) return;
 
       input.classList[input.value ? 'add' : 'remove']('_filled');
     });
